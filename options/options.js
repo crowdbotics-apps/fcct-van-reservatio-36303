fadda7e81fdd1c8f25362e97b293d957d17a4771
Crowdbotics <team@crowdbotics.com>
2022-07-19 21:54:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "fcct_van_reservatio_36303",
+  url: "https://fcct_van_reservatio_36303.botics.co",
+  api: "https://fcct_van_reservatio_36303.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
